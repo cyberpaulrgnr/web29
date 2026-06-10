2ab4bde20f401e28dcbec01823b8d829ff2526ea
cyberpaulrgnr <2061108@aup.edu.ph>
2026-06-10 21:44:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let sendFeedbackBtn = document.getElementById("sendFeedbackBtn");
-let yourName = document.getElementById("yourName");
-let email = document.getElementById("email");
-let yourThoughts = document.getElementById("yourThoughts");
+import { useState } from 'react';
 
-sendFeedbackBtn.addEventListener("click", () => {
-    let yourNameValue = yourName.value;
-    let emailValue = email.value;
-    let yourThoughtsValue = yourThoughts.value;
+export default function ContactForm() {
+  const [result, setResult] = useState("");
 
-    if (yourName.value == "") {
-        alert(`You should put something in your name`);
-    } else if (email.value == "") {
-        alert("You should put something in email address");
-    } else if (yourThoughts.value == "") {
-        alert("You should put something in your thoughts");
-    } else {
-        alert(`The sender's name is ${yourNameValue} and his/her email is ${emailValue} and he/she wants to say to you ${yourThoughtsValue}`);
-    }
-})
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "2070bba2-e914-4cf5-acdd-ba3fb3d398bf");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" required/>
+      <input type="email" name="email" required/>
+      <textarea name="message" required></textarea>
+      <button type="submit">Submit</button>
+      <p>{result}</p>
+    </form>
+  );
+}
