f985182b566fc8e14c90de03a598461d931ac60d
cyberpaulrgnr <2061108@aup.edu.ph>
2026-06-11 19:10:41 +0800

    try to update to web29/resume.docx

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement('a');
-    link.href = "resume.docx";
+    link.href = "/web29/resume.docx";
     link.download = "resume.docx";
     document.body.appendChild(link);
     link.click();
