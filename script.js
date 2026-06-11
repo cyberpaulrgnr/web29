8255e3d59233a78b47cfc42cdc38e6bdc469075e
cyberpaulrgnr <2061108@aup.edu.ph>
2026-06-11 19:13:22 +0800

    Update the downloading resume to direct url

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement('a');
-    link.href = "/web29/resume.docx";
+    link.href = "https://cyberpaulrgnr.github.io/web29/resume.docx";
     link.download = "resume.docx";
     document.body.appendChild(link);
     link.click();
